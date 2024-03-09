f7319f7a3b05b686f408d9a99d7689798674c046
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-03-09 16:13:40 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="vietnam">
+        <div className="card">
+          {" "}
+          <p>HELLO</p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
