90006402478ce414d3f508b072d7e3dd85568cda
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-03-11 14:40:08 +0100

    add slider buttons

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,19 +1,38 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
 
 import { images } from "./images";
 
 import "./slider.css";
 import "swiper/css";
+import { useRef } from "react";
 
 const Slider = () => {
+  const sliderRef = useRef<SwiperRef>(null);
+
+  const handleNext = () => {
+    sliderRef.current?.swiper.slideNext();
+  };
+
   return (
     <div className="grid-item slider-container">
-      <Swiper slidesPerView={"auto"} loop speed={1400} className="swiper">
+      <Swiper
+        ref={sliderRef}
+        slidesPerView={"auto"}
+        loop
+        speed={1400}
+        className="swiper"
+      >
         {images.map((slide, index) => (
           <SwiperSlide className="slide" key={index}>
             <img className="slide-image" src={slide.image}></img>
           </SwiperSlide>
         ))}
+        <button className="see-more-button" onClick={handleNext}>
+          See more photos
+        </button>
+        <button className="arrow-button" onClick={handleNext}>
+          <img src="./slider-arrow.svg" />
+        </button>
       </Swiper>
     </div>
   );
