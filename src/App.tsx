42eccc2bbb24cced87b8efc98b9c3b099a932c79
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-03-11 16:26:54 +0100

    small changes in code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import Top from "./components/top/Top";
 import Title from "./components/title/Title";
 import Text from "./components/text/Text";
@@ -8,13 +6,8 @@ import Slider from "./components/slider/Slider";
 import "./App.css";
 
 function App() {
-  const [showImage, setShowImage] = useState<boolean>(false);
-
   return (
     <section id="vietnam">
-      {showImage ? (
-        <img className="task-image" src="./Graficke-zadani.png" />
-      ) : null}
       <div className="card">
         <Top />
         <Title />
@@ -25,13 +18,6 @@ function App() {
           <p>More Short Trips</p>
         </a>
       </div>
-
-      <button
-        onClick={() => setShowImage(!showImage)}
-        className="show-image-button"
-      >
-        Show task Image
-      </button>
     </section>
   );
 }
